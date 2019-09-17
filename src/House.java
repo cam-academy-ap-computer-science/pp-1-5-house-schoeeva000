e6d1076a7e08e5580bbc4ce94be6e96876969435
wooki <wooki@HowlingPirates>
2019-09-16 21:08:23 -0700

    Name: Evan Schoening Date: 9/16/19 House that Jack built

--- src/House.java
+++ src/House.java
@@ -2,8 +2,58 @@
 public class House {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("This is the house that Jack built.\n");
+		System.out.println("This is the malt");
+		jb();
+		System.out.println("This is the rat,");
+		malt();
+		jb();
+		System.out.println("This is the cat,");
+		rat();
+		malt();
+		jb();
+		System.out.println("This is the dog,");
+		cat();
+		rat();
+		malt();
+		jb();
+		System.out.println("This is the cow with the crumpled horn,");
+		dog();
+		cat();
+		rat();
+		malt();
+		jb();
+		System.out.println("This is the maiden all forlorn,");
+		cow();
+		dog();
+		cat();
+		rat();
+		malt();
+		jb();
+		
 	}
 
+	public static void jb() {
+		System.out.println("That lay in the house that jack built.\n");
+	}
+	
+	public static void malt() {
+		System.out.println("That ate the malt,");
+	}
+	
+	public static void rat() {
+		System.out.println("That killed the rat,");
+	}
+	
+	public static void cat() {
+		System.out.println("That worried the cat,");
+	}
+	
+	public static void dog() {
+		System.out.println("That tossed the dog,");
+	}
+	
+	public static void cow() {
+		System.out.println("That milked the cow with the crumpled horn,");
+	}
 }
